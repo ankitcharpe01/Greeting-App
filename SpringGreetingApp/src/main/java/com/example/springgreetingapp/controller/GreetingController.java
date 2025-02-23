dc6c92561ea9e1ade132169aeecc9e83a9d227fa
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-02-23 13:00:49 +0530

    [Ankit Charpe]Add.Added personalized greeting for UC3

--- SpringGreetingApp/src/main/java/com/example/springgreetingapp/controller/GreetingController.java
+++ SpringGreetingApp/src/main/java/com/example/springgreetingapp/controller/GreetingController.java
@@ -3,12 +3,12 @@ package com.example.springgreetingapp.controller;
 import com.example.springgreetingapp.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-
     private final GreetingService greetingService;
 
     // Constructor-based Dependency Injection
@@ -17,7 +17,8 @@ public class GreetingController {
     }
 
     @GetMapping
-    public String getGreeting() {
-        return "{\"message\": \"" + greetingService.getGreetingMessage() + "\"}";
+    public String getGreeting(@RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName) {
+        return "{\"message\": \"" + greetingService.getGreetingMessage(firstName, lastName) + "\"}";
     }
 }
--- SpringGreetingApp/src/main/java/com/example/springgreetingapp/service/GreetingService.java
+++ SpringGreetingApp/src/main/java/com/example/springgreetingapp/service/GreetingService.java
@@ -5,8 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 
-    public String getGreetingMessage() {
-        return "Hello!! Have a nice day";  // Business logic
+    public String getGreetingMessage(String firstName, String lastName) {
+        if (firstName != null && lastName != null) {
+            return "Hello, " + firstName + " " + lastName + "!";
+        } else if (firstName != null) {
+            return "Hello, " + firstName + "!";
+        } else if (lastName != null) {
+            return "Hello, " + lastName + "!";
+        } else {
+            return "Hello World!";
+        }
     }
 }
 
